9ec55e697b9b92e678f026724812987233768701
ramez <ramezkhaled259@gmail.com>
2025-09-24 22:23:39 +0300

    Finishing a header part with dropdown menu component, the search bar with fetching data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import DropdownMenu from "./components/DropdownMenu";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [searchCity, setSearchCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!searchCity) return;
+
+    async function fetchWeather() {
+      try {
+        setLoading(true);
+        setError(null);
+
+        const geoRes = await fetch(
+          `https://geocoding-api.open-meteo.com/v1/search?name=${searchCity}&count=1`
+        );
+        const geoData = await geoRes.json();
+        console.log(geoData);
+
+        if (!geoData.results) {
+          setError("City not found!");
+          setLoading(false);
+          return;
+        }
+
+        const { latitude, longitude } = geoData.results[0];
+
+        const weatherRes = await fetch(
+          `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`
+        );
+        const weatherData = await weatherRes.json();
+        console.log(weatherData);
+
+        setWeather(weatherData.current_weather);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchWeather();
+  }, [searchCity]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <HeroSection>
+        <h1>How's the sky looking today?</h1>
+        <SearchBar
+          city={city}
+          setCity={setCity}
+          setSearchCity={setSearchCity}
+        />
+      </HeroSection>
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <img className="logo" src="./assets/images/logo.svg" alt="" />
+
+      <DropdownMenu />
+    </header>
+  );
+}
+
+function HeroSection({ children }) {
+  return <div className="hero-sec">{children}</div>;
+}
+
+function SearchBar({ city, setCity, setSearchCity }) {
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (city.trim() !== "") {
+      setSearchCity(city);
+    }
+  }
+
+  return (
+    <form className="search-bar" onSubmit={handleSubmit}>
+      <img
+        className="search-icon"
+        src="./assets/images/icon-search.svg"
+        alt="search"
+      />
+      <input
+        type="text"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        placeholder="Search for a city..."
+      />
+      <button type="submit" onClick={handleSubmit}>
+        Search
+      </button>
+    </form>
   );
 }
 
---
+++ src/components/DropdownMenu.js
@@ -0,0 +1,104 @@
+// src/components/DropdownMenu.jsx
+import { useState } from "react";
+
+const units = [
+  {
+    secLabel: "Temperature",
+    metricVal: "Celsius (°C)",
+    imperialVal: "Fahrenheit (°F)",
+  },
+  { secLabel: "Wind Speed", metricVal: "Km/h", imperialVal: "Mph" },
+  {
+    secLabel: "Precipitation",
+    metricVal: "Millimeters (mm)",
+    imperialVal: "Inches (in)",
+  },
+];
+
+function DropdownMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isImperial, setIsImperial] = useState(false);
+
+  function handleSwitch() {
+    setIsImperial(!isImperial);
+  }
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="dropdown-container">
+      <button className="dropdown-button" onClick={toggleMenu}>
+        <img src="./assets/images/icon-units.svg" alt="units" />
+        Units
+        <img src="./assets/images/icon-dropdown.svg" alt="units dropdown" />
+      </button>
+
+      <div className={`dropdown-menu ${isOpen ? "show" : ""}`}>
+        <h5 className="switch-imperial" value="imperial" onClick={handleSwitch}>
+          Switch to {isImperial ? "Metric" : "Imperial"}
+        </h5>
+
+        {units.map((unit) => (
+          <DropdownSection
+            key={unit.secLabel}
+            secLabel={unit.secLabel}
+            metricVal={unit.metricVal}
+            imperialVal={unit.imperialVal}
+            isImperial={isImperial}
+          />
+        ))}
+
+        {/* <div className="dropdown-section">
+          <h6 className="dropdown-label">Temperature</h6>
+          <div className="dropdown-item selected">
+            Celsius (°C)
+            <img src="./assets/images/icon-checkmark.svg" alt="selected" />
+          </div>
+          <div className="dropdown-item">Fahrenheit (°F)</div>
+        </div>
+
+        <div className="dropdown-section">
+          <h6 className="dropdown-label">Wind Speed</h6>
+          <div className="dropdown-item selected">
+            km/h
+            <img src="./assets/images/icon-checkmark.svg" alt="selected" />
+          </div>
+          <div className="dropdown-item">mph</div>
+        </div>
+
+        <div className="dropdown-section">
+          <h6 className="dropdown-label">Precipitation</h6>
+          <div className="dropdown-item selected">
+            Millimeters (mm)
+            <img src="./assets/images/icon-checkmark.svg" alt="selected" />
+          </div>
+          <div className="dropdown-item">Inches (in)</div>
+        </div> */}
+      </div>
+    </div>
+  );
+}
+
+function DropdownSection({ secLabel, metricVal, imperialVal, isImperial }) {
+  return (
+    <div className="dropdown-section">
+      <h6 className="dropdown-label">{secLabel}</h6>
+      <div className={`dropdown-item ${isImperial ? "selected" : ""}`}>
+        {metricVal}
+        {isImperial && (
+          <img src="./assets/images/icon-checkmark.svg" alt="selected" />
+        )}
+      </div>
+      <div className={`dropdown-item ${!isImperial ? "selected" : ""}`}>
+        {imperialVal}
+        {!isImperial && (
+          <img src="./assets/images/icon-checkmark.svg" alt="selected" />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default DropdownMenu;
